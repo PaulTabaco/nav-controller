a983ecf8ed00db28b509def3153ec0540a158e0b
Paul <comrade_pasha@ukr.net>
2016-12-05 22:55:08 +0200

    v001

---
+++ nav-controller/YellowVC.swift
@@ -0,0 +1,40 @@
+//
+//  YellowVC.swift
+//  nav-controller
+//
+//  Created by Paul on 04.01.16.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+import UIKit
+
+class YellowVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func popCV(sender: AnyObject) {
+        navigationController?.popViewControllerAnimated(true)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
